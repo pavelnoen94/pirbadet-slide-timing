64a975be97eb33eff54cfaa4d246bb8ff94307a3
Pavel Skipenes <pavelgs@stud.ntnu.no>
2020-06-15 08:06:14 +0100

    implemented timing

---
+++ src/Rider.py
@@ -0,0 +1,13 @@
+import time
+
+class Rider:
+    time = None
+
+    def start_time(self):
+        self.time = time.time()
+
+    def get_time(self):
+        return time.time() - self.time
+
+    def get_speed(self, distance):
+        return distance/self.get_time()
