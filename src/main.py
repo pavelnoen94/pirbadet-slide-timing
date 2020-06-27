6bf860d67103593b7c6ad821dc4783065945c6e1
Pavel Skipenes <pavelgs@stud.ntnu.no>
2020-06-27 23:17:24 +0200

    Implemented highscores with the rest of the program. Added SIGTERM and SIGINT handlers.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-import Slide, threading
+import Slide, threading, signal, multiprocessing
+
+terminator = multiprocessing.Event()
 
 
 def main():
@@ -7,19 +9,23 @@ def main():
     # load configurations
     configurations = ["white_slide.ini"]
     threads = []
-
     # load all slides in threads and start
     for configuration in configurations:
-        threads.append(threading.Thread(target=Slide.Slide, args=[configuration]))
+        threads.append(threading.Thread(target=Slide.Slide, args=[configuration, terminator]))
 
     for thread in threads:
         thread.start()
 
-    for thread in threads:
+    signal.signal(signal.SIGTERM, terminator_handler)
+    signal.signal(signal.SIGINT, terminator_handler)
+
+    if thread.is_alive():
         thread.join()
 
     return
 
+def terminator_handler(arg1, arg2):
+    terminator.set()
 
 if __name__ == "__main__":
     main()
