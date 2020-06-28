5903197cae2c85b281be3b0f969abe209e6aa28b
Pavel Skipenes <pavelgs@stud.ntnu.no>
2020-06-29 00:34:29 +0200

    - Implemented messaging system. Slide now sends status and data with high scores packed inside json object. Slides can also recieve commands to reset, disable and enable it self. - Fixed a handfull of bugs when in weird situations (double reset, disable while someone is sliding, reset while disabled etc...) - Refactored code inside highscores to use less hard coded variables - Probably a lot more as well but I'm tired and going to bed now. Good night.

--- src/highscores.py
+++ src/highscores.py
@@ -1,134 +1,124 @@
 from datetime import datetime as date
-import pickle
+import pickle, os, json
 
 
 class high_scores:
 
 
-    def __init__(self, name):
+    def __init__(self, name, mqtt):
         self.backup_path = name + ".highscores.pkl"
-        self.last = (None, None)
-        self.today = (None, None)
-        self.week = (None, None)
-        self.month = (None, None)
-        self.best_ever = (None, None)
+        self.init_highscores()
         self.load_highscores()
-
-        return
-
-
-    def add_time(self, time):
-        if (time == None):
-            return
-
-        self.last = (time, date.now())
-
-        try:
-            if (time < self.today[0]):
-                self.today = (time, date.now())
-            else:
-                return
-        except TypeError:
-            self.today = (time, date.now())
-
-        try:
-            if (time < self.week[0]):
-                self.week = (time, date.now())
-            else:
-                return
-        except TypeError:
-            self.week = (time, date.now())
-
-        try:
-            if (time < self.month[0]):
-                self.month = (time, date.now())
-            else:
-                return
-        except TypeError:
-            self.month = (time, date.now())
-
-        try:
-            if (time < self.best_ever[0]):
-                self.best_ever = (time, date.now())
-        except TypeError:
-            self.best_ever = (time, date.now())
-
-        return
-
-
-    def update_highscores(self):
-        # compare todays date and current highscores.
-        # clear out highscores that are out of date
-
-        try:
-            if (self.today[1].day != date.now().day):
-                self.today = (None, None)
-        except AttributeError:
-            self.today = (None, None)
-        try:
-            if (self.week[1].isocalendar()[1] != date.now.isocalendar()[1]):
-                self.week = (None, None)
-        except AttributeError:
-            self.week = (None, None)
-        try:
-            if (self.month[1].month != date.now.month()):
-                self.month = (None, None)
-        except AttributeError:
-            self.month = (None, None)
-
-        return
-
-
-    def save_highscores(self):
-        with open(self.backup_path, "wb") as f:
-            pickle.dump(self, f)
-
-        return
-
-
-    def clear_all(self):
-        self.last = (None, None)
-        self.today = (None, None)
-        self.week = (None, None)
-        self.month = (None, None)
-        self.best_ever = (None, None)
+        self.mqtt = mqtt
 
         return
 
 
     def load_highscores(self):
         try:
-            with open(self.backup_path, "rb") as f:
-                temp = pickle.load(f)
-                self.last = (None, None)
-                self.today = temp.today
-                self.week = temp.week
-                self.month = temp.month
-                self.best_ever = temp.best_ever
-                self.update_highscores()
-        except FileNotFoundError:
-            pass
+            if os.path.getsize(self.backup_path) > 0:
+                with open(self.backup_path, "rb") as f:
+                    temp = pickle.load(f)
+                    self.highscores = temp
+                    self.update_highscores()
+        except (FileNotFoundError) as e:
+            print(e)
+            # TODO: delete the file if it is present
+
+            return
+
+
+    def save_highscores(self):
+        with open(self.backup_path, "wb") as f:
+            pickle.dump(self.highscores, f)
+        return
+
+
+    def update_highscores(self):
+        # clear out highscores that are "out of date"
+
+        now = date.now()
+
+        if (self.highscores["today"]["date"].day == now.day):
+            return
+
+        self.highscores["today"] =  {
+            "time" : float(),
+            "date" : now,
+            "speed" : float()
+            }
+
+        if (self.highscores["week"]["date"].isocalendar()[1] == now.isocalendar()[1]):
+            return
+
+        self.highscores["week"] = {
+            "time" : float(),
+            "date" : now,
+            "speed" : float()
+            }
+
+        if (self.highscores["month"]["date"].month == now.month):
+            return
+
+        self.highscores["month"] =  {
+            "time" : float(),
+            "date" : now,
+            "speed" : float()
+            }
 
         return
 
 
-    def print_highscores(self):
-        print()
-        print("highscores:")
+    def add_result(self, time, speed):
+        if (time == None):
+            return
 
-        if (self.last[0] != None):
-            print("last: " + str(self.last[0]) +"s")
+        # update all records if the time is better
+        for i in self.highscores:
+            if(time < self.highscores[i]["time"] or self.highscores[i]["time"] == float()):
+                self.highscores[i]["time"] = time
+                self.highscores[i]["speed"] = speed
+                self.highscores[i]["date"] = date.now()
 
-        if (self.today[0] != None):
-            print("today: " + str(self.today[0]) +"s")
+        # update last time
+        self.highscores["last"]["time"] = time
+        self.highscores["last"]["speed"] = speed
+        self.highscores["last"]["date"] = date.now()
 
-        if (self.week[0] != None):
-            print("week: " + str(self.week[0]) +"s")
+        return
 
-        if (self.month[0] != None):
-            print("month: " + str(self.month[0]) +"s")
 
-        if (self.best_ever[0] != None):
-            print("best_ever: " + str(self.best_ever[0]) +"s")
-
+    def init_highscores(self):
+        self.highscores =   {
+                    "last" :
+                        {
+                            "time"  : float(),
+                            "date"  : date.now(),
+                            "speed" : float()
+                        },
+                    "today" :
+                        {
+                            "time"  : float(),
+                            "date"  : date.now(),
+                            "speed" : float()
+                        },
+                    "week" :
+                        {
+                            "time"  : float(),
+                            "date"  : date.now(),
+                            "speed" : float()
+                        },
+                    "month" :
+                        {
+                            "time"  : float(),
+                            "date"  : date.now(),
+                            "speed" : float()
+                        },
+                    "ever" :
+                        {
+                            "time"  : float(),
+                            "date"  : date.now(),
+                            "speed" : float()
+                        }
+                    }
         return
